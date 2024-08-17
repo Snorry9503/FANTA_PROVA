78e213565c2df65aa27375108115b1e05e271b60
Snorry9503 <gabrielerosato869@gmail.com>
2024-08-17 14:29:15 +0200

    modifiche varie alle logiche

--- app.py
+++ app.py
@@ -9,10 +9,4 @@ app=Flask(__name__)
 def index():
  return "<p>Indice, cosa vuoi ?</p>"
 
-@app.route("/db")
-def dbShow():
-    if __name__ == 'main':
-       return f"<h1>questa è la parte if {__name__}</h1>"   
-    else:
-       return f"<h1>questa è la parte else {__name__}</h1>"
 
