3ca8868dddaab0f26362e7acb8a908eab037c7de
Gabriele <gabrielerosato869@gmail.com>
2024-08-18 18:12:22 +0200

    Prime logiche dell'API

--- app.py
+++ app.py
@@ -10,4 +10,22 @@ app=Flask(__name__)
 def index():
  return "<p>Indice, cosa vuoi ?</p>"
 
+@app.route("/dbIngest", method="[GET, POST]")
+def dbingest():
+    return "<p>codice Ingest database</p>"
 
+@app.route("/getGiocatore/<id>", method="[GET]")
+def getGiocatore():
+   return "<p>Ritorna il giocatore</p>"
+
+@app.route("/getGiocatori", method="[GET]")
+def getGiocatori():
+   return "<p>Ritorna tutta la lista dei giocatori</p>"
+
+@app.route("/setSquadraFantaGiocatore/<id>", method="[PUT]")
+def setFantaSquadra():
+   return "<p>Fa un update del giocatore e lo assegna ad una Fantasquadra</p>"
+
+@app.route("/setIngaggioGiocatore/<id>", method="[PUT]")
+def setIngaggioGiocarore():
+   return "<p>Setta l'ingaggio che ha pagato il fantagiocatore per la squadra</p>"
